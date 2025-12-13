627ec7f874a315aeafd3ebe36a084d416318c966
Karib Suleymanov <suleimanovcarib@yandex.ru>
2025-12-13 23:50:55 +0300

    практическая 9, исправлены ошибки

--- src/components/View/Forms/ContactsForm.ts
+++ src/components/View/Forms/ContactsForm.ts
@@ -19,18 +19,23 @@ export class ContactsFormView extends Form<IContactsFormData> {
             'input[name="email"]',
             this.container
         );
+
         this.phoneInput = ensureElement<HTMLInputElement>(
             'input[name="phone"]',
             this.container
         );
 
-        this.emailInput.addEventListener('input', () =>
-            this.events.emit('contacts:update', { email: this.emailInput.value })
-        );
+        this.emailInput.addEventListener('input', () => {
+            this.events.emit('contacts:update', {
+                email: this.emailInput.value,
+            });
+        });
 
-        this.phoneInput.addEventListener('input', () =>
-            this.events.emit('contacts:update', { phone: this.phoneInput.value })
-        );
+        this.phoneInput.addEventListener('input', () => {
+            this.events.emit('contacts:update', {
+                phone: this.phoneInput.value,
+            });
+        });
 
         this.container.addEventListener('submit', (e) => {
             e.preventDefault();
@@ -44,19 +49,6 @@ export class ContactsFormView extends Form<IContactsFormData> {
 
     set phone(value: string) {
         this.phoneInput.value = value ?? '';
-    }
-
-    protected getData(): IContactsFormData {
-        return {
-            email: this.emailInput.value,
-            phone: this.phoneInput.value,
-            errors: {}
-        };
-    }
-
-    setData(data: Partial<IContactsFormData>): void {
-        if (data.email !== undefined) this.email = data.email;
-        if (data.phone !== undefined) this.phone = data.phone;
     }
 
     clear(): void {
